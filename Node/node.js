5c1193078288f389195a05ca05713970a0dd08d9
Anton von Weltzien <anton.weltzien@gmail.com>
2017-11-21 14:22:02 +0100

    Beautify the code

--- Node/node.js
+++ Node/node.js
@@ -1,12 +1,12 @@
-const net = require("net");
+const net = require("net")
 
 // Create a socket (client) that connects to the server
-console.log("Script runs good");
-var socket = new net.Socket();
+console.info("Script runs good")
+const socket = new net.Socket()
 socket.connect(2020, "localhost", function () {
-  console.log("Client: Connected to server");
-  socket.write(JSON.stringify({ response: "Hey there server!" }));
-// Close the connection
-  socket.end();
-});
+  console.info("Client: Connected to server")
+  socket.write(JSON.stringify({ response: "Hey there server!" }))
+  socket.end()
+  //hallo
+})
 
