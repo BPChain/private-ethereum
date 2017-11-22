48d091c5d37a60c5d977dd71ed3de69abb39b6e1
jcremerius <jonas.cremerius@student.hpi.de>
2017-11-22 14:22:31 +0100

    add an blockchain account in each node

--- Node/files/start.sh
+++ Node/files/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 geth --datadir=~/.ethereum/devchain init "/root/files/blockchain_files/genesis.json"
+geth --datadir=~/.ethereum/devchain --password <(echo -n 123)  account new
 BOOTSTRAP_IP=`getent hosts bootstrap | cut -d" " -f1`
 GETH_OPTS=${@/IPAddress/$BOOTSTRAP_IP}
 echo 'Schranz'
