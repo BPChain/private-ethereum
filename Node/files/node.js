9947228392a708843caad4ce24642f6e28f5f3b9
Anton von Weltzien <anton.weltzien@gmail.com>
2017-11-22 15:54:34 +0100

    add package.json

--- Node/files/node.js
+++ Node/files/node.js
@@ -3,6 +3,8 @@ const Web3 = require("web3")
 // Create a socket (client) that connects to the server
 console.info("Script runs good and great")
 const socket = new net.Socket()
+const web3 = new Web3("ws://localhost:8546")
+console.info(web3)
 
 socket.connect(2020, "localhost", function () {
   console.info("Client: Connected to server")
