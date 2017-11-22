ab30bd1e378d303b9e72bcf3c9ec9510b9b586b3
jcremerius <jonas.cremerius@student.hpi.de>
2017-11-22 15:41:24 +0100

    add web3 library to node.js

--- Node/files/node.js
+++ Node/files/node.js
@@ -1,8 +1,9 @@
 const net = require("net")
-
+const Web3 = require("web3")
 // Create a socket (client) that connects to the server
 console.info("Script runs good and great")
 const socket = new net.Socket()
+
 socket.connect(2020, "localhost", function () {
   console.info("Client: Connected to server")
   socket.write(JSON.stringify({ response: "Hey there server!" }))
