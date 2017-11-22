a3db3d8a97e21381a553f4de99f9f010f335a2a8
jcremerius <jonas.cremerius@student.hpi.de>
2017-11-22 13:12:57 +0100

    add all files required for a succesffull blockchain connection

--- Node/files/start.sh
+++ Node/files/start.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-cd /root/files
-node node.js
+geth --datadir=~/.ethereum/devchain init "/root/files/blockchain_files/genesis.json"
+BOOTSTRAP_IP=`getent hosts bootstrap | cut -d" " -f1`
+GETH_OPTS=${@/IPAddress/$BOOTSTRAP_IP}
+echo 'Schranz'
+echo $GETH_OPTS
+geth $GETH_OPTS
+#node /root/files/node.js
