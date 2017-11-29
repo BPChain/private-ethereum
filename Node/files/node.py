99e48f32c5ee5707191c39f8574958cba082672a
Anton von Weltzien <anton.weltzien@gmail.com>
2017-11-29 10:55:31 +0100

    refactor name shadowing and simplify connect_to_blockchain method

--- Node/files/node.py
+++ Node/files/node.py
@@ -1,20 +1,13 @@
-import json
-import web3
 import time
-from web3 import Web3, HTTPProvider, TestRPCProvider
+from web3 import Web3, HTTPProvider
 import requests
 
 
 def connect_to_blockchain():
-    connected = False
-    while not connected:
-        try:
-            time.sleep(10)
-            web3 = Web3(HTTPProvider('http://localhost:8545'))
-            return web3
-        except Exception:
-            pass
-        connected = True
+    web3 = Web3(HTTPProvider('http://localhost:8545'))
+    while not web3.isConnected():
+        time.sleep(1)
+    return web3
 
 
 def start_mining(web3):
@@ -29,10 +22,10 @@ def retrieve_last_blocks(number_of_last_sent_block, web3):
         number_of_blocks_to_send = number_of_last_block - number_of_last_sent_block
         for i in range(0, number_of_blocks_to_send):
             last_blocks.append(web3.eth.getBlock(number_of_last_block - i))
-        return(number_of_last_block, last_blocks)
+        return (number_of_last_block, last_blocks)
     else:
         print("Nothing to send")
-        return(number_of_last_sent_block, last_blocks)
+        return (number_of_last_sent_block, last_blocks)
 
 
 # get the avg block difficulty of the current send cycle
@@ -60,7 +53,8 @@ def provide_data(web3):  # Loop, which runs on the nodes to get and send the dat
         node_id = web3.admin.nodeInfo.id
         hash_rate = web3.eth.hashrate
         gas_price = web3.eth.gasPrice
-        node_data = {'node_id': node_id, 'hashrate': hash_rate, 'gas_price': gas_price, 'Avg Block difficulty': avg_block_difficulty}
+        node_data = {'node_id': node_id, 'hashrate': hash_rate, 'gas_price': gas_price,
+                     'Avg Block difficulty': avg_block_difficulty}
         print(node_data)
         send_data(node_data)
 
@@ -74,8 +68,9 @@ def send_data(node_data):  # send the data to the server
         print("Connection has not been established")
         pass
 
+
 if __name__ == "__main__":
-    web3 = connect_to_blockchain()
-    start_mining(web3)
-    provide_data(web3)
+    web3_connector = connect_to_blockchain()
+    start_mining(web3_connector)
+    provide_data(web3_connector)
 
