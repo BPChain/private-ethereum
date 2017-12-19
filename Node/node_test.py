1b058621c4cfa6e721de36fbc191b22d51650074
Anton von Weltzien <anton.weltzien@gmail.com>
2017-12-19 12:01:31 +0100

    remove global config field

--- Node/node_test.py
+++ Node/node_test.py
@@ -1,21 +1,24 @@
 import pytest
 import sys
+
 sys.path.insert(0, 'files')
 import node
 from attrdict import AttrDict
 
 last_sent_block = AttrDict({'blockId': '0', 'difficulty': 2.0, 'timestamp': 1})
 blocks = [AttrDict({'blockId': '1', 'difficulty': 2.0, 'timestamp': 2}),
-        AttrDict({'blockId': '2', 'difficulty': 3.0, 'timestamp': 3}),
-        AttrDict({'blockId': '3', 'difficulty': 4.0, 'timestamp': 4})]
+          AttrDict({'blockId': '2', 'difficulty': 3.0, 'timestamp': 3}),
+          AttrDict({'blockId': '3', 'difficulty': 4.0, 'timestamp': 4})]
 
 
-def avg_difficulty():
+def test_avg_difficulty():
     assert node.calculate_avg_block_difficulty(blocks) == 3
 
-def avg_blockTime():
+
+def test_avg_block_time():
     assert node.calculate_avg_block_time(blocks, last_sent_block) == 1
 
+
 if __name__ == "__main__":
-    avg_difficulty()
-    avg_blockTime()
+    test_avg_difficulty()
+    test_avg_block_time()
