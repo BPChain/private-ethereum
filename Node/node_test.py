d6371200ca8b48f2859de566e957806477a1fea2
jcremerius <jonas.cremerius@student.hpi.de>
2017-12-19 11:43:04 +0100

    add methods for testing

--- Node/node_test.py
+++ Node/node_test.py
@@ -9,5 +9,13 @@ blocks = [AttrDict({'blockId': '1', 'difficulty': 2.0, 'timestamp': 2}),
         AttrDict({'blockId': '2', 'difficulty': 3.0, 'timestamp': 3}),
         AttrDict({'blockId': '3', 'difficulty': 4.0, 'timestamp': 4})]
 
-assert node.calculate_avg_block_difficulty(blocks) == 3
-assert node.calculate_avg_block_time(blocks, last_sent_block) == 1
+
+def avg_difficulty():
+    assert node.calculate_avg_block_difficulty(blocks) == 3
+
+def avg_blockTime():
+    assert node.calculate_avg_block_time(blocks, last_sent_block) == 1
+
+if __name__ == "__main__":
+    avg_difficulty()
+    avg_blockTime()
