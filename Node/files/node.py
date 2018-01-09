3670c3d872fb61a67f42f67c120500c48fe2def5
yT0n1 <anton.weltzien@gmail.com>
2018-01-09 17:20:49 +0100

    Merge tests and updated codestyle into dev (#10)
    * hopefully add nvidia-docker support
    
    * remove global config field
    
    * add node_test.py so tests can be written
    
    * add debug
    
    * add debug
    
    * add tests and Exception Handling if the server is not reachable
    
    * add methods for testing
    
    * remove global config field
    
    *  Move test file into test folder
    
    * Add comment to test
    
    * Add config file for pylint and fix code smells
    
    * add printing of the exception
    
    * add correct exception printing
    
    * Add pythonic module structure and simplify travis workflow slightly.
    
    * add logging in node.py
    
    * modify the exception message
    
    * fix code style
    
    * modify the exception message
    
    * fix the logging
    
    * fix the logging
    
    * modify the timeout parameter of the create connection command
    
    * remove the magic number and introduces a variable
    
    * simplify send_data function
    
    * add higher level requirements txt which also contains requirements for tests.

--- Node/files/node.py
+++ Node/files/node.py
@@ -1,21 +1,24 @@
-"""I read values from my local blockchain node by using the Web3 Library which uses the nodes RPC-API. Make sure the
-    required apis are unlocked in geth. I send the values I read to a server in a specified interval via a http post.
-    Important variables are SERVER_ADDRESS and SEND_PERIOD. The values I send can currently be seen in gather_data"""
+""" I read values from my local blockchain node by using the Web3 Library which uses the nodes
+    RPC-API.
+    Make sure the required apis are unlocked in geth. I send the values I read to a server in a
+    specified interval via a http post. Important variables are SERVER_ADDRESS and SEND_PERIOD.
+    The values I send can currently be seen in gather_data"""
 
-import time
-from web3 import Web3, HTTPProvider
-from functools import reduce
-from websocket import create_connection
 import json
+import logging
+import time
+from functools import reduce
+
 import yaml
-config = yaml.safe_load(open("/root/files/config.yml"))
+from web3 import Web3, HTTPProvider
+from websocket import create_connection, WebSocket
+
 
 def connect_to_blockchain():
     web3 = Web3(HTTPProvider('http://localhost:8545'))
     while not web3.isConnected():
         time.sleep(1)
     return web3
-
 
 
 def start_mining(web3):
@@ -39,12 +42,13 @@ def calculate_avg_block_difficulty(blocks_to_send):
     if not blocks_to_send:
         return 0
     else:
-        return reduce((lambda accum, block: accum + block.difficulty), blocks_to_send, 0) / len(blocks_to_send)
+        return reduce((lambda accum, block: accum + block.difficulty), blocks_to_send, 0) / len(
+            blocks_to_send)
 
 
 def calculate_avg_block_time(blocks_to_send, last_sent_block):
     blocks_to_send = [last_sent_block] + blocks_to_send
-    #first block might be genesis block with timestamp 0. this has to be catched.
+    # first block might be genesis block with timestamp 0. this has to be catched.
     if len(blocks_to_send) == 1:
         return 0
     else:
@@ -52,12 +56,14 @@ def calculate_avg_block_time(blocks_to_send, last_sent_block):
             blocks_to_send.remove(None)
         if len(blocks_to_send) == 1:
             return 0
-        deltas = [next.timestamp - current.timestamp for current, next in zip(blocks_to_send, blocks_to_send[1:])]
+        deltas = [next.timestamp - current.timestamp for current, next in
+                  zip(blocks_to_send, blocks_to_send[1:])]
         return sum(deltas) / len(deltas)
 
 
 def provide_data_every(n_seconds, web3):
     last_block_number = 0
+    uri = yaml.safe_load(open("/root/files/config.yml"))
     while True:
         time.sleep(n_seconds)
         last_sent_block = web3.eth.getBlock(last_block_number) if last_block_number > 0 else None
@@ -65,7 +71,7 @@ def provide_data_every(n_seconds, web3):
         last_block_number = new_last_block_number
         node_data = gather_data(blocks_to_send, last_sent_block, web3)
         print(node_data)
-        send_data(node_data)
+        send_data_to(uri, node_data)
 
 
 def gather_data(blocks_to_send, last_sent_block, web3):
@@ -76,27 +82,44 @@ def gather_data(blocks_to_send, last_sent_block, web3):
     gas_price = web3.eth.gasPrice
     is_mining = 1 if web3.eth.mining else 0
     node_data = {"hostId": host_id, "hashrate": hash_rate, "gasPrice": gas_price,
-                 "avgDifficulty": avg_block_difficulty, "avgBlocktime": avg_block_time, "isMining": is_mining}
+                 "avgDifficulty": avg_block_difficulty, "avgBlocktime": avg_block_time,
+                 "isMining": is_mining}
     return node_data
 
 
-def send_data(node_data):
-    ws = create_connection(
-        config['networking']['socketProtocol'] +
-        config['networking']['socketAdress'] +
+def web_socket_for(uri) -> WebSocket:
+    timeout_in_seconds = 10
+    web_socket = create_connection(
+        uri['networking']['socketProtocol'] +
+        uri['networking']['socketAdress'] +
         ":" +
-        config['networking']['socketPort']
+        uri['networking']['socketPort'], timeout_in_seconds
     )
-    ws.send(json.dumps(node_data))
-    print("Sent")
-    print("Receiving...")
-    result = ws.recv()
-    print("Received '%s'" % result)
-    ws.close()
+    logging.critical({"message": "Connection established"})
+    return web_socket
+
+
+def send_data_to(uri, node_data):
+    try:
+        web_socket = web_socket_for(uri)
+        web_socket.send(json.dumps(node_data))
+        print("Sent\nReceiving...")
+        result = web_socket.recv()
+        print("Received '%s'" % result)
+        logging.critical({"message": result})
+        web_socket.close()
+    # Not nice, but works for now.
+    # pylint: disable=broad-except
+    except Exception as exception:
+        print("Exception occurred during sending: ")
+        print(exception)
+        logging.critical({"message": exception})
 
 
 if __name__ == "__main__":
-    SEND_PERIOD  = 10
-    web3_connector = connect_to_blockchain()
-    start_mining(web3_connector)
-    provide_data_every(SEND_PERIOD, web3_connector)
+    logging.basicConfig(filename='private_ethereum.log', level=logging.CRITICAL,
+                        format='%(asctime)s %(message)s')
+    SEND_PERIOD = 10
+    WEB3_CONNECTOR = connect_to_blockchain()
+    start_mining(WEB3_CONNECTOR)
+    provide_data_every(SEND_PERIOD, WEB3_CONNECTOR)
--- Node/files/start_eth_node.sh
+++ Node/files/start_eth_node.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+nvidia-smi
 geth --datadir=~/.ethereum/devchain init "/root/files/blockchain_files/genesis.json"
 geth --datadir=~/.ethereum/devchain --password <(echo -n 123)  account new
 BOOTSTRAP_IP=`getent hosts bootstrap | cut -d" " -f1`
