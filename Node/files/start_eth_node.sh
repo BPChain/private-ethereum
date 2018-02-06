e0446889676ab460ca3f8c8d4cec685ec1da04ca
jcremerius <jonas.cremerius@student.hpi.de>
2018-02-06 15:05:47 +0100

    Remove the nvidia volumes

--- Node/files/start_eth_node.sh
+++ Node/files/start_eth_node.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-nvidia-smi
 geth --datadir=~/.ethereum/devchain init "/root/files/blockchain_files/genesis.json"
 geth --datadir=~/.ethereum/devchain --password <(echo -n 123)  account new
 BOOTSTRAP_IP=`getent hosts bootstrap | cut -d" " -f1`
