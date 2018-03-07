b1737fcd8d3d3d5c828bc3370ff0c98e90cfa1a3
jcremerius <jonas.cremerius@student.hpi.de>
2018-03-07 16:17:30 +0100

    Automize the migration of the smart contract

--- Node/files/EVAPcoin/startMigration.js
+++ Node/files/EVAPcoin/startMigration.js
@@ -3,8 +3,10 @@ const execa = require("execa")
 const id = setInterval(function () {
 execa('truffle', ['migrate', '--network=dev']).then(function (result) {
         console.log(result)
-    }).catch(function (){
+        process.exit(0)
+    }).catch(function (error){
         console.log("Error while migrating")
+        console.log(error)
     })
 
 }, 10000)
