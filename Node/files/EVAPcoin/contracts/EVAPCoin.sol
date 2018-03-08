49c841b39dc77d853a3f6ef070fab4d4cabf3dbf
jcremerius <jonas.cremerius@student.hpi.de>
2018-03-08 11:14:04 +0100

    Modify the solidity code so we have an array of all registered students

--- Node/files/EVAPcoin/contracts/EVAPCoin.sol
+++ Node/files/EVAPcoin/contracts/EVAPCoin.sol
@@ -3,7 +3,7 @@ contract EVAPCoin {
 
     mapping(address => uint256) allUser;
     mapping(address => string) roles;
-    address[] students;
+    address[] public students;
 
     address master;
 
@@ -31,9 +31,10 @@ contract EVAPCoin {
     function registerAsStudent() public hasRole("") returns (bool success) {
         roles[msg.sender] = 'student';
         students.push(msg.sender);
+        success = true;
     }
 
-    function getStudents() public hasRole("master") returns (address[] students) {
+    function getStudents() public hasRole("master") returns(address[]) {
         return students;
     }
 
