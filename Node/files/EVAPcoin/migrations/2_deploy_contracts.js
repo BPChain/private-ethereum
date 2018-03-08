81ae1884b1fe365552fdd4234a02cb730177f563
jcremerius <jonas.cremerius@student.hpi.de>
2018-03-08 14:27:12 +0100

    change the deploy script, so execa gets executed

--- Node/files/EVAPcoin/migrations/2_deploy_contracts.js
+++ Node/files/EVAPcoin/migrations/2_deploy_contracts.js
@@ -5,16 +5,17 @@ const execa = require("execa")
 const EVAPCoin = artifacts.require("../contracts/EVAPCoin.sol");
 
 module.exports = function(deployer) {
-
+    var address
     return deployer.deploy(EVAPCoin).then(function () {
         return EVAPCoin.deployed()
             .then(function (instance) {
-                console.log("Before execa")
-                execa('truffle', ['exec', '../scripts/master/generate.js','--network=dev']).then(function () {  })
-                console.log("After execa")
-                return instance.address
-            })
-            .then(function (address) {
+                address = instance.address
+                return address
+            }).then(function () {
+                    console.log("Before execa")
+                    return execa('truffle', ['exec', '../scripts/master/generate.js','--network=dev'])
+            }).then(function () {
+                console.log("Spawn server")
                 const wsServer = new WebSocketServer({port: 40000})
                 wsServer.on('connection', function (connection) {
                     connection.send(address)
