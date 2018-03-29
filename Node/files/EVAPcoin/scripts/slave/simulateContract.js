18d206ff4f913906c43db994d567c0ba8a2ba8e3
jcremerius <jonas.cremerius@student.hpi.de>
2018-03-29 16:36:45 +0200

    Modify simulateContract so that nodes which do not have suffiecient funds retry to become a student

--- Node/files/EVAPcoin/scripts/slave/simulateContract.js
+++ Node/files/EVAPcoin/scripts/slave/simulateContract.js
@@ -1,11 +1,19 @@
 const execa = require("execa")
 module.exports = function (address, iterationTime) {
-    return execa('truffle', ['exec', 'becomeStudent.js', address, '--network=dev']).then(function (result) {
-        console.log(result)
-        return result
-    }).then(function () {
-        require("./simulateTransactions")(address, iterationTime)
-    }).then(function () {
+    var interval = setInterval(function() {
+        try {
+            return execa('truffle', ['exec', 'becomeStudent.js', address, '--network=dev']).then(function (result) {
+                console.log(result)
+                return result
+            }).then(function () {
+                clearInterval(interval)
+                require("./simulateTransactions")(address, iterationTime)
+            }).then(function () {
 
-    })
+            })
+        } catch (error) {
+            console.log("Become Student failed")
+        }
+    }, 30000)
 }
+
