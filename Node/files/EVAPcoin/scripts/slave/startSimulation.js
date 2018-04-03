df1a563f46bce35cb1e984add2c234c875325840
jcremerius <jonas.cremerius@student.hpi.de>
2018-04-03 11:15:34 +0200

    Modify simulateContract so that nodes which do not have suffiecient funds retry to become a student

--- Node/files/EVAPcoin/scripts/slave/startSimulation.js
+++ Node/files/EVAPcoin/scripts/slave/startSimulation.js
@@ -5,21 +5,19 @@ function start() {
   var ws
     ws = new WebSocket('ws://eth_contract_deployer:40000')
      ws.on('message', function incoming(address) {
-          console.log("-------------------------Addresse-------------")
+          console.log("-------------------------Address-------------")
           console.log(address)
           require("./simulateContract")(address, iterationTime).catch(function (error) {
               setTimeout(function () {
-          console.log("Become student failed");
+          console.log("Starting simulation failed");
           console.log(error)
           start()
         }, 10000)
          })
      })
   ws.onerror=function(event) {
-
       setTimeout(function () {
           console.log("Contract address WebSocket not reachable");
-          start()
         }, 10000)
   }
 
