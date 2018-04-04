031d4027303f0a3971330606ecedd3ea78394f60
jcremerius <jonas.cremerius@student.hpi.de>
2018-04-04 13:52:19 +0200

    Modify the port to connect to bootnode

--- Node/files/EVAPcoin/truffle.js
+++ Node/files/EVAPcoin/truffle.js
@@ -2,7 +2,7 @@ module.exports = {
     networks: {
         dev: {
             host: "127.0.0.1",
-            port: 8545,
+            port: 8546,
             network_id: "*",
             gas: 4712388
         }
