2b35a27d492b188f9beae863ca70e95ae2bd083e
jcremerius <jonas.cremerius@student.hpi.de>
2018-04-04 14:04:04 +0200

    Modify the port to connect to bootnode

--- Node/files/start_eth_node.sh
+++ Node/files/start_eth_node.sh
@@ -3,6 +3,7 @@ geth --datadir=~/.ethereum/devchain init "/root/files/blockchain_files/genesis.j
 geth --datadir=~/.ethereum/devchain --password <(echo -n 123)  account new
 BOOTSTRAP_IP=`getent hosts bootstrap | cut -d" " -f1`
 GETH_OPTS=${@/IPAddress/$BOOTSTRAP_IP}
+echo GETH_OPTS
 python3 /root/files/node.py &
 geth $GETH_OPTS &
 cd /root/files/EVAPcoin/scripts/slave && node /root/files/EVAPcoin/scripts/slave/startSimulation.js 20000
