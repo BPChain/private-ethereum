3d54859ea5f3a02ad948c964c0d81267ca3956ca
jcremerius <jonas.cremerius@student.hpi.de>
2018-04-09 13:24:51 +0200

    fix node test

--- Node/tests/node_test.py
+++ Node/tests/node_test.py
@@ -44,7 +44,7 @@ def test_provide_data_for_first_block():
     web3.eth.getBlock = getBlock
     number_of_last_block = 0
     node_data = {"avgDifficulty": 0, "avgBlocktime": 0}
-    last_b_num, node_data = node.provide_data(number_of_last_block, node_data, web3)
+    last_b_num, node_data = node.provide_data(number_of_last_block, node_data, web3, 'test')
     assert last_b_num == 3
     assert node_data['avgDifficulty'] == 0
     assert node_data['avgBlocktime'] == 0
@@ -59,7 +59,7 @@ def test_provide_data():
     web3.eth.getBlock = getBlock
     number_of_last_block = 1
     node_data = {"avgDifficulty": 0, "avgBlocktime": 0}
-    last_b_num, node_data = node.provide_data(number_of_last_block, node_data, web3)
+    last_b_num, node_data = node.provide_data(number_of_last_block, node_data, web3, 'test')
     assert last_b_num == 3
     assert node_data['avgDifficulty'] == 3.5
     assert node_data['avgBlocktime'] == 1
@@ -74,7 +74,7 @@ def test_provide_data_no_new_blocks():
     web3.eth.getBlock = getBlock
     number_of_last_block = BLOCKS[0].number
     node_data = {"avgDifficulty": 3, "avgBlocktime": 4.4}
-    last_b_num, node_data = node.provide_data(number_of_last_block, node_data, web3)
+    last_b_num, node_data = node.provide_data(number_of_last_block, node_data, web3, 'test')
     assert last_b_num == BLOCKS[0].number
     assert node_data['avgDifficulty'] == 3
     assert node_data['avgBlocktime'] == 4.4
@@ -114,7 +114,7 @@ def test_node_data_format():
     web3.eth.getBlock = getBlock
     number_of_last_block = BLOCKS[0].number
     node_data = {"avgDifficulty": 3, "avgBlocktime": 4.4}
-    _, node_data = node.provide_data(number_of_last_block, node_data, web3)
+    _, node_data = node.provide_data(number_of_last_block, node_data, web3, 'test')
     demo_data = {"hostId": 1, "hashrate": 1, "gasPrice": 1,
                  "avgDifficulty": 1, "avgBlocktime": 1,
                  "isMining": 1}
