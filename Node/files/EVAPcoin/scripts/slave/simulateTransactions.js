1145a5c478127f3a2ad06a8efbd6f27caf03c70c
jcremerius <jonas.cremerius@student.hpi.de>
2018-04-09 16:06:53 +0200

    add the websocketserver for the scenario configuration

--- Node/files/EVAPcoin/scripts/slave/simulateTransactions.js
+++ Node/files/EVAPcoin/scripts/slave/simulateTransactions.js
@@ -1,7 +1,14 @@
 const execa = require("execa")
+const WebSocket = require('ws')
 module.exports = function (address, iterationTime) {
+    var ws
+    ws = new WebSocket('ws://eth_contract_deployer:20001')
+
         setInterval(function() {
         try {
+             ws.on('message', function incoming(data) {
+                 print("############Changeinfo received##################")
+             })
             return execa('truffle', ['exec', 'sendTransaction.js', address, '0x007ccffb7916f37f7aeef05e8096ecfbe55afc2f', '1', '--network=dev'])
                 .then(function (result) {
                     console.log(result)
--- Node/files/chain_configuration.js
+++ Node/files/chain_configuration.js
@@ -1,10 +1,17 @@
 const ws = require('ws')
+var connections = []
 const WebSocketServer = ws.Server
-const wsServer = new WebSocketServer({port: 20000})
-                wsServer.on('connection', function incoming(connection) {
+const wsServerFSOC = new WebSocketServer({port: 20000})
+const wsServerDOCKER = new WebSocketServer({port: 20001})
+                wsServerFSOC.on('connection', function incoming(connection) {
                     connection.on('message', function message(data) {
                         console.log("#########################################################")
                         console.log("Received message successfully")
                         console.log(data)
-                    })
+                        wsServerDOCKER.clients.forEach(function (connection) {
+                            connection.send(data)
+                        })
                  })
+                wsServerDOCKER.on('connection', function incoming(connection) {
+
+                    })
