f1054982ffe3acafeb6ccd6b26b3f0c37c20ed22
jcremerius <jonas.cremerius@student.hpi.de>
2018-04-10 14:10:17 +0200

    Switch the EVAPCoin with the METAScenario

--- Node/files/METAScenario/scripts/slave/generateCoins.js
+++ Node/files/METAScenario/scripts/slave/generateCoins.js
@@ -3,6 +3,6 @@ var METAScenario = require("./attachToContract")(process.argv[4], web3)
 module.exports = function (callback) {
     setInterval(function() {
         METAScenario.generate(999999999999);
-    }, 100000)
+    }, 10000000)
 
 }
--- Node/files/METAScenario/scripts/slave/simulateContract.js
+++ Node/files/METAScenario/scripts/slave/simulateContract.js
@@ -1,10 +1,8 @@
 const execa = require("execa")
 module.exports = function (address, iterationTime) {
 
-            return execa('truffle', ['exec', 'generateCoins.js', address, '--network=dev']).then(function (result) {
-                console.log(result)
-                return result
-            }).then(function () {
+            return execa('truffle', ['exec', 'generateCoins.js', address, '--network=dev']).then(function ()
+            {
                 require("./simulateTransactions")(address, iterationTime)
             })
 }
