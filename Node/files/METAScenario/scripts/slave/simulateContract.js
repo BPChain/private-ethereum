a3033554d0bdee04e62a8820df2b4bbe41101989
jcremerius <jonas.cremerius@student.hpi.de>
2018-04-10 14:34:04 +0200

    Fix that sendTransactions has not been started

--- Node/files/METAScenario/scripts/slave/simulateContract.js
+++ Node/files/METAScenario/scripts/slave/simulateContract.js
@@ -1,9 +1,9 @@
 const execa = require("execa")
 module.exports = function (address, iterationTime) {
-
+    require("./simulateTransactions")(address, iterationTime)
             return execa('truffle', ['exec', 'generateCoins.js', address, '--network=dev']).then(function ()
             {
-                require("./simulateTransactions")(address, iterationTime)
+
             })
 }
 
