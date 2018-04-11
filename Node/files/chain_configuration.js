7177a07618e32bb5ceb075eecfc8b87c1b3ede54
jcremerius <jonas.cremerius@student.hpi.de>
2018-04-11 16:03:13 +0200

    Make the code prettier

--- Node/files/chain_configuration.js
+++ Node/files/chain_configuration.js
@@ -5,28 +5,28 @@ const wsServerFSOC = new WebSocketServer({port: 20000})
 const wsServerDOCKER = new WebSocketServer({port: 20001})
 var number_of_connections = 0
 var cached_data = JSON.stringify({"payloadSize": 10, "frequency": 20})
-setInterval(function () {
-    if (number_of_connections < wsServerDOCKER.clients.size) {
-        number_of_connections = wsServerDOCKER.clients.size
-        wsServerDOCKER.clients.forEach(function (connection) {
-                            connection.send(cached_data)
-                        })
-    }
-}, 30000)
-                wsServerFSOC.on('connection', function incoming(connection) {
-                    connection.on('message', function message(data) {
-                        console.log("#########################################################")
-                        console.log("Received message successfully")
-                        console.log(data)
-                        cached_data = data
-                        console.log(wsServerDOCKER.clients)
-                        console.log(wsServerDOCKER.clients.size)
-                        number_of_connections = wsServerDOCKER.clients.size
-                        wsServerDOCKER.clients.forEach(function (connection) {
-                            connection.send(data)
-                        })
-                    })
-                })
-                wsServerDOCKER.on('connection', function incoming(connection) {
+    setInterval(function () {
+        if (number_of_connections < wsServerDOCKER.clients.size) {
+            number_of_connections = wsServerDOCKER.clients.size
+            wsServerDOCKER.clients.forEach(function (connection) {
+                                connection.send(cached_data)
+                            })
+        }
+    }, 30000)
+    wsServerFSOC.on('connection', function incoming(connection) {
+        connection.on('message', function message(data) {
+            console.log("#########################################################")
+            console.log("Received message successfully")
+            console.log(data)
+            cached_data = data
+            console.log(wsServerDOCKER.clients)
+            console.log(wsServerDOCKER.clients.size)
+            number_of_connections = wsServerDOCKER.clients.size
+            wsServerDOCKER.clients.forEach(function (connection) {
+                connection.send(data)
+            })
+        })
+    })
+    wsServerDOCKER.on('connection', function incoming(connection) {
 
-                    })
+        })
