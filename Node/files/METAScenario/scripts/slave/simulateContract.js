68da75e00bcaa930aae4358545a76f0a9162c9e4
jcremerius <jonas.cremerius@student.hpi.de>
2018-04-11 14:17:18 +0200

    Modify the scenario calls so there are no multiple sendTransactions instances

--- Node/files/METAScenario/scripts/slave/simulateContract.js
+++ Node/files/METAScenario/scripts/slave/simulateContract.js
@@ -1,6 +1,5 @@
 const execa = require("execa")
 module.exports = function (address, iterationTime) {
         require("./simulateTransactions")(address, iterationTime)
-            return execa('truffle', ['exec', 'generateCoins.js', address, '--network=dev']).then(function () {})
 }
 
--- Node/files/METAScenario/scripts/slave/startSimulation.js
+++ Node/files/METAScenario/scripts/slave/startSimulation.js
@@ -1,17 +1,21 @@
 const WebSocket = require('ws');
 const iterationTime = process.argv[2]
+const execa = require("execa")
 
-function start() {
+function start(error_flag) {
   var ws
     ws = new WebSocket('ws://eth_contract_deployer:40000')
      ws.on('message', function incoming(address) {
           console.log("-------------------------Address-------------")
           console.log(address)
+         if(error_flag === 0) {
+             generateCoins()
+         }
           require("./simulateContract")(address, iterationTime).catch(function (error) {
               setTimeout(function () {
           console.log("Starting simulation failed");
           console.log(error)
-          start()
+          start(1)
         }, 10000)
          })
      })
@@ -22,10 +26,22 @@ function start() {
 
   ws.onclose=function(event){
       setTimeout(function () {
-          start()
+          start(0)
         }, 10000)
   }
 
 }
-start()
+
+function generateCoins() {
+        try {
+            return execa('truffle', ['exec', 'generateCoins.js', address, '--network=dev']).then(function () {
+            })
+        } catch(error)
+        {
+            setTimeout(function () {
+                generateCoin()
+            }, 10000)
+        }
+}
+start(0)
 
