f5a603c64f53da9f65178c3450ea3dd52496b421
jcremerius <jonas.cremerius@student.hpi.de>
2018-04-11 11:33:09 +0200

    Remove console output

--- Node/files/METAScenario/scripts/slave/simulateTransactions.js
+++ Node/files/METAScenario/scripts/slave/simulateTransactions.js
@@ -4,18 +4,14 @@ const randomBytes = require('random-bytes')
 module.exports = function (address, interval) {
 
     var ws
-    var bytes_to_send = 0x0000000000000000000000000000000000000000000000000000000001020304
+    var bytes_to_send = randomBytes.sync(1)
     ws = new WebSocket('ws://eth_contract_deployer:20001')
     var intervalID
 
     ws.on('message', function incoming(data) {
-        console.info("############Changeinfo received##################")
-        console.info(data)
         clearInterval(intervalID)
         var newInterval = JSON.parse(data).frequency
         var newPayloadSize = JSON.parse(data).payloadSize
-        console.info(newInterval)
-        console.info(newPayloadSize)
         bytes_to_send = randomBytes.sync(newPayloadSize)
         startInterval(newInterval, bytes_to_send)
     })
@@ -24,9 +20,6 @@ module.exports = function (address, interval) {
 
       intervalID = setInterval(function() {
         try {
-            console.info(_bytes_to_send)
-            console.info("&&&&&&&&&&&INTERVAL&&&&&&&&&&&&")
-            console.info(_interval)
             return execa('truffle', ['exec', 'sendTransaction.js', address, '0x007ccffb7916f37f7aeef05e8096ecfbe55afc2f', '1', _bytes_to_send, '--network=dev'])
                 .then(function (result) {
                     console.log(result)
