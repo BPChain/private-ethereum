55d5f8adaf95a7cb05256f24db0cc4a7dff2d39c
jcremerius <jonas.cremerius@student.hpi.de>
2018-04-12 14:31:16 +0200

    Remove debug output

--- Node/files/chain_configuration.js
+++ Node/files/chain_configuration.js
@@ -22,8 +22,6 @@ var cached_data = JSON.stringify({"payloadSize": 10, "period": 20})
             console.log("Received message successfully")
             console.log(data)
             cached_data = data
-            console.log(wsServerDOCKER.clients)
-            console.log(wsServerDOCKER.clients.size)
             number_of_connections = wsServerDOCKER.clients.size
             wsServerDOCKER.clients.forEach(function (connection) {
                 connection.send(data)
