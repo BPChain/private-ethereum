94e0cc392ee17e1ba8b625e77a5dce0dbdbc3a23
jcremerius <jonas.cremerius@student.hpi.de>
2018-04-12 14:17:05 +0200

    Add dag so it does not have to be precomputed every time and add a timeout when the contract migration failed

--- Node/files/METAScenario/startMigration.js
+++ Node/files/METAScenario/startMigration.js
@@ -5,9 +5,12 @@ execa('truffle', ['migrate', '--network=dev']).then(function (result) {
         console.log(result)
         process.exit(0)
     }).catch(function (error){
-        console.log("Error while migrating")
-        console.log(error)
-        start()
+        setTimeout(function () {
+            console.log("Error while migrating")
+            console.log(error)
+            start()
+        }, 15000)
+
     })
 
 }
