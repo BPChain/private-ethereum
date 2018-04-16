0ec3eb469c0e280e494e4c8fee68c753001f4f20
jcremerius <jonas.cremerius@student.hpi.de>
2018-04-16 15:00:36 +0200

    Add debug info

--- Node/files/METAScenario/scripts/slave/sendTransaction.js
+++ Node/files/METAScenario/scripts/slave/sendTransaction.js
@@ -1,6 +1,6 @@
 var METAScenario = require("./attachToContract")(process.argv[4], web3)
-var receiver = process.argv[5];
-var value = process.argv[6];
+var receiver = process.argv[5]
+var value = process.argv[6]
 var data = process.argv[7]
 module.exports = function (address) {
     var result = METAScenario.transfer(receiver, value, data);
