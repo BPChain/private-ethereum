483f721db9674a80ee49d469792d68f4e7f17294
jcremerius <jonas.cremerius@student.hpi.de>
2018-04-17 15:54:05 +0200

    Add a timeout when there is an init error. This happens while scaling when the contract is already deployed

--- Node/files/METAScenario/scripts/slave/simulateTransactions.js
+++ Node/files/METAScenario/scripts/slave/simulateTransactions.js
@@ -10,16 +10,22 @@ module.exports = function (address, interval) {
     var abi = JSON.parse('[{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"students","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_value","type":"uint256"},{"indexed":false,"name":"_data","type":"bytes"}],"name":"Transfer","type":"event"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"myBalance","outputs":[{"name":"myBalance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_value","type":"uint256"}],"name":"generate","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"},{"name":"_data","type":"bytes"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"printAddress","outputs":[{"name":"self","type":"address"}],"payable":false,"stateMutability":"view","type":"function"}]')
 
     function initialize() {
+        try {
+            var provider = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8547"));
+            var METAScenario = provider.eth.contract(abi).at(address);
+            provider.eth.defaultAccount = provider.eth.accounts[0];
+            var x = METAScenario.transfer('0x007ccffb7916f37f7aeef05e8096ecfbe55afc2f', 1, bytes_to_send.toString('hex'))
+            console.log(x)
+            console.log(METAScenario)
+            startws(METAScenario)
+            startInterval(interval, bytes_to_send, METAScenario)
+        } catch(error) {
+            setTimeout(function () {
+                initialize()
+            }, 20000)
+        }
 
-        var provider = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8547"));
-        var METAScenario = provider.eth.contract(abi).at(address);
-        provider.eth.defaultAccount = provider.eth.accounts[0];
-        var x = METAScenario.transfer('0x007ccffb7916f37f7aeef05e8096ecfbe55afc2f', 1, bytes_to_send.toString('hex'))
-        console.log(x)
-        console.log(METAScenario)
 
-        startws(METAScenario)
-        startInterval(interval, bytes_to_send, METAScenario)
 
     }
 
