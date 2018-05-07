37b6afbed02f28f3bb66c9a1692d1c99411ddef4
Anton von Weltzien <anton.weltzien@gmail.com>
2018-05-07 13:23:40 +0200

    Change name

--- Node/files/METAScenario/scripts/slave/startSimulation.js
+++ Node/files/METAScenario/scripts/slave/startSimulation.js
@@ -2,36 +2,37 @@ const WebSocket = require('ws');
 const iterationTime = process.argv[2]
 const execa = require("execa")
 
-function start() {
-  var ws
+function receiveContractAddress() {
+    var ws
     ws = new WebSocket('ws://eth_contract_deployer:40000')
-     ws.on('message', function incoming(address) {
-          console.log("-------------------------Address-------------")
-          console.log(address)
-             generateCoins(address)
-          require("./simulateTransactions")(address, iterationTime)
-     })
-  ws.onerror=function(event) {
-          console.log("Contract address WebSocket not reachable");
-          ws.close()
-  }
+    ws.on('message', function incoming(address) {
+        console.log("-------------------------Address-------------")
+        console.log(address)
+        generateCoins(address)
+        require("./simulateTransactions")(address, iterationTime)
+    })
+    ws.onerror = function (event) {
+        console.log("Contract address WebSocket not reachable");
+        ws.close()
+    }
 
-  ws.onclose=function(event){
-      setTimeout(function () {
-          start()
+    ws.onclose = function (event) {
+        setTimeout(function () {
+            receiveContractAddress()
         }, 10000)
-  }
+    }
 
 }
 
 function generateCoins(address) {
 
-            return execa('truffle', ['exec', 'generateCoins.js', address, '--network=dev']).catch(function () {
-                setTimeout(function () {
-                generateCoins(address)
-            }, 10000)
-            })
+    return execa('truffle', ['exec', 'generateCoins.js', address, '--network=dev']).catch(function () {
+        setTimeout(function () {
+            generateCoins(address)
+        }, 10000)
+    })
 
 }
-start()
+
+receiveContractAddress()
 
