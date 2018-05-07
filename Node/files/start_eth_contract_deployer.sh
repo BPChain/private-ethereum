2bc7a0799c36d1901a82d40ebca06fe3aa744d98
Anton von Weltzien <anton.weltzien@gmail.com>
2018-05-07 15:51:56 +0200

    Fix start_deployer

--- Node/files/start_eth_contract_deployer.sh
+++ Node/files/start_eth_contract_deployer.sh
@@ -8,7 +8,6 @@ geth $GETH_OPTS &
 cd /root/files/METAScenario;
 node startMigration.js &
 cd /root/files/scripts/python_sources
-pip install -r requirements.txt
 python3 -m python_sources.master_node.run_threads
 
 
