f9e1f69a0c5085a6a2ff03bc9b69cfb3fb16eb57
Anton von Weltzien <anton.weltzien@gmail.com>
2018-05-08 13:13:54 +0200

    Add send mechanics

--- Node/files/METAScenario/scripts/python_sources/implementation/Slave.py
+++ Node/files/METAScenario/scripts/python_sources/implementation/Slave.py
@@ -1,6 +1,7 @@
-
 from ..implementation import Setup
 from ..project_logger import set_up_logging
+
+from websocket import create_connection, WebSocket
 
 LOG = set_up_logging(__name__)
 
@@ -9,16 +10,26 @@ class Slave:
     def __init__(self, config, setup: Setup):
         LOG.info(config)
         LOG.info(setup)
-        pass
+        try:
+            self.web_socket = create_connection('ws://' + config['ipAddress'], 5)
+            LOG.info("Sent data")
+            # result = web_socket.recv()
+            # LOG.info("Received '%s'", result)
+            # web_socket.close()
+        except Exception as error:
+            LOG.error(error)
 
     def is_alive(self):
-        pass
+        try:
+            self.web_socket.ping()
+            return True
+        except Exception as error:
+            LOG.error(error)
+            return False
 
     def transact(self, name, hex_string):
-        pass
+        self.web_socket.send(hex_string)
 
     @classmethod
     def get_new(cls, config, setup: Setup):
         return Slave(config, setup)
-
-
