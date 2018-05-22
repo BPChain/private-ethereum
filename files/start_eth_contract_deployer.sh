7e306aaaf4a8362cd3ab630aba06cab171af19bf
jcremerius <jonas.cremerius@student.hpi.de>
2018-05-22 11:07:25 +0200

    Rename eth_bootstrap and add separate script for contract deployer where is_mining is always 0 to display the correct number of miners

--- files/start_eth_contract_deployer.sh
+++ files/start_eth_contract_deployer.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 geth --datadir=~/.ethereum/devchain init "/root/files/blockchain_files/genesis.json"
 geth --datadir=~/.ethereum/devchain --password <(echo -n 123)  account new
-BOOTSTRAP_IP=`getent hosts e_bootstrap | cut -d" " -f1`
+BOOTSTRAP_IP=`getent hosts eth_bootstrap | cut -d" " -f1`
 GETH_OPTS=${@/IPAddress/$BOOTSTRAP_IP}
-python3 -m root.files.node &
+python3 -m root.files.contract_deployer &
 geth $GETH_OPTS &
 cd /root/files/METAScenario;
 node startMigration.js &
