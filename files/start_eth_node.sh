ab00d886610044df5e234f3b4c24d896db5cd193
jcremerius <jonas.cremerius@student.hpi.de>
2018-05-22 10:45:20 +0200

    Change eht_bootstrap to e_bootstrap

--- files/start_eth_node.sh
+++ files/start_eth_node.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 geth --datadir=~/.ethereum/devchain init "/root/files/blockchain_files/genesis.json"
 geth --datadir=~/.ethereum/devchain --password <(echo -n 123)  account new
-BOOTSTRAP_IP=`getent hosts eth_bootstrap | cut -d" " -f1`
+BOOTSTRAP_IP=`getent hosts e_bootstrap | cut -d" " -f1`
 GETH_OPTS=${@/IPAddress/$BOOTSTRAP_IP}
 python3 -m root.files.node &
 geth $GETH_OPTS &
