bdcd0e6c0081b98747f486979fdefb9815484756
jcremerius <jonas.cremerius@student.hpi.de>
2018-05-24 11:28:35 +0200

    Remove web3admin and correct the web3 port

--- files/METAScenario/scripts/runTransactionSlave.js
+++ files/METAScenario/scripts/runTransactionSlave.js
@@ -2,12 +2,11 @@ const WebSocketServer = require('ws').Server
 const Web3 = require('web3')
 const request = require('request')
 const ip = require('ip')
-const web3Admin = require('web3admin')
 const fs = require('fs')
 
 module.exports = function (address) {
 
-    var provider = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
+    var provider = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8547"));
     var abi = JSON.parse('[{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"students","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_value","type":"uint256"},{"indexed":false,"name":"_data","type":"bytes"}],"name":"Transfer","type":"event"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"myBalance","outputs":[{"name":"myBalance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_value","type":"uint256"}],"name":"generate","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"},{"name":"_data","type":"bytes"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"printAddress","outputs":[{"name":"self","type":"address"}],"payable":false,"stateMutability":"view","type":"function"}]')
 
     function initialize() {
