ae9e5c4bfb1ac81242263c8ee63ca3fdb93dc513
jcremerius <jonas.cremerius@student.hpi.de>
2018-05-24 11:22:08 +0200

    Fix directory path for simulation script

--- files/start_eth_node.sh
+++ files/start_eth_node.sh
@@ -5,7 +5,7 @@ BOOTSTRAP_IP=`getent hosts eth_bootstrap | cut -d" " -f1`
 GETH_OPTS=${@/IPAddress/$BOOTSTRAP_IP}
 python3 -m root.files.node 1 &
 geth $GETH_OPTS &
-cd /root/files/METAScenario/scripts/slave &&
+cd /root/files/METAScenario/scripts/ &&
 node /root/files/METAScenario/scripts/startSimulation.js
 
 
