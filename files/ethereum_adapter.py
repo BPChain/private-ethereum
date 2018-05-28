cb5247f11018d3af9e65daa384edce031b8f16b4
Anton von Weltzien <anton.weltzien@gmail.com>
2018-05-28 13:54:26 +0200

    Fix name of web3 to web3_rpc

--- files/ethereum_adapter.py
+++ files/ethereum_adapter.py
@@ -9,12 +9,12 @@ class EthereumAdapter(BlockchainAdapter):
 
     def __init__(self, is_miner):
         super().__init__(is_miner)
-        self.web3 = Web3(HTTPProvider('http://127.0.0.1:8547',
-                                 request_kwargs={'timeout': 120}))
-        while not self.web3.isConnected():
+        self.web3_rpc = Web3(HTTPProvider('http://127.0.0.1:8547',
+                                          request_kwargs={'timeout': 120}))
+        while not self.web3_rpc.isConnected():
             sleep(1)
-        self.web3.personal.unlockAccount(self.web3.eth.accounts[0], "123", 0)
-        self.web3.miner.start(1)
+        self.web3_rpc.personal.unlockAccount(self.web3_rpc.eth.accounts[0], "123", 0)
+        self.web3_rpc.miner.start(1)
 
     def fetch_newest_block_number(self) -> int:
         return self.web3_rpc.eth.getBlock('latest').number
