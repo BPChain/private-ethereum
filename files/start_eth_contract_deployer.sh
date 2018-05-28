ae11e2ca4f3636656902de1f0ffac4cddf5010f7
Anton von Weltzien <anton.weltzien@gmail.com>
2018-05-28 16:13:39 +0200

    Statistics readout (#28)
    * Switch to package architecture
    
    * Update statistics version
    
    * Update statistics version
    
    * Fix name of web3 to web3_rpc
    
    * Should fix build
    
    * Fix linting

--- files/start_eth_contract_deployer.sh
+++ files/start_eth_contract_deployer.sh
@@ -3,7 +3,7 @@ geth --datadir=~/.ethereum/devchain init "/root/files/blockchain_files/genesis.j
 geth --datadir=~/.ethereum/devchain --password <(echo -n 123)  account new
 BOOTSTRAP_IP=`getent hosts eth_bootstrap | cut -d" " -f1`
 GETH_OPTS=${@/IPAddress/$BOOTSTRAP_IP}
-python3 -m root.files.node 0 &
+python3 -m root.files.data_collection 0 &
 geth $GETH_OPTS &
 cd /root/files/METAScenario;
 node startMigration.js &
